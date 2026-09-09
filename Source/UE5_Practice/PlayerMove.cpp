6ffa78f02b7a09a548c460009d46d36d351745ad
KimSungJun <tjdwns1314@gmail.com>
2026-09-09 11:04:32 +0900

    feat: PlayerMove 컴포넌트로 이동 로직 옮김

--- Source/UE5_Practice/PlayerMove.cpp
+++ Source/UE5_Practice/PlayerMove.cpp
@@ -2,4 +2,87 @@
 
 
 #include "PlayerMove.h"
+#include "EnhancedInputComponent.h"
+#include "InputActionValue.h"
+
+void UPlayerMove::SetupInputBinding(UEnhancedInputComponent* PlayerInput)
+{
+	PlayerInput->BindAction(ia_Turn, ETriggerEvent::Triggered, this, &UPlayerMove::Turn);
+	PlayerInput->BindAction(ia_LookUp, ETriggerEvent::Triggered, this, &UPlayerMove::LookUp);
+	PlayerInput->BindAction(ia_Move, ETriggerEvent::Triggered, this, &UPlayerMove::Move);
+	PlayerInput->BindAction(ia_Run, ETriggerEvent::Started, this, &UPlayerMove::InputRun);
+	PlayerInput->BindAction(ia_Run, ETriggerEvent::Completed, this, &UPlayerMove::InputRun);
+	PlayerInput->BindAction(ia_Jump, ETriggerEvent::Started, this, &UPlayerMove::InputJump);
+}
+
+void UPlayerMove::Move(const FInputActionValue& inputValue)
+{
+	FVector2D value = inputValue.Get<FVector2D>();
+	direction.X = value.X;
+	direction.Y = value.Y;
+}
+
+void UPlayerMove::PlayerMove()
+{
+	direction = FTransform(me->GetControlRotation()).TransformVector(direction);
+	me->AddMovementInput(direction);
+	direction = FVector::ZeroVector;
+}
+
+
+UPlayerMove::UPlayerMove()
+{
+	//Tick 함수 호출되도록 처리
+	PrimaryComponentTick.bCanEverTick = true;
+}
+
+void UPlayerMove::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	// 초기 속도를 걷기로 설정
+	moveComp->MaxWalkSpeed = walkSpeed;
+}
+
+void UPlayerMove::TickComponent(float DeltaTime, ELevelTick TickType, FActorComponentTickFunction * ThisTickFunction)
+{
+	Super::TickComponent(DeltaTime, TickType, ThisTickFunction);
+
+	PlayerMove();
+}
+
+void UPlayerMove::Turn(const FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	me->AddControllerYawInput(value);
+
+}
+
+void UPlayerMove::LookUp(const FInputActionValue& inputValue)
+{
+	float value = inputValue.Get<float>();
+	me->AddControllerPitchInput(value);
+}
+
+void UPlayerMove::InputRun()
+{
+	auto movement = me->GetCharacterMovement();
+
+	BRunning = !BRunning;
+
+	if (BRunning)
+	{
+		movement->MaxWalkSpeed = runSpeed;
+	}
+	else
+	{
+		movement->MaxWalkSpeed = walkSpeed;
+		BIsRunShooting = false;
+	}
+}
+
+void UPlayerMove::InputJump(const FInputActionValue& inputValue)
+{
+	me->Jump();
+}
 
--- Source/UE5_Practice/PlayerMove.h
+++ Source/UE5_Practice/PlayerMove.h
@@ -14,4 +14,91 @@ class UE5_PRACTICE_API UPlayerMove : public UPlayerBaseComponent
 {
 	GENERATED_BODY()
 
+public:
+	UPlayerMove();
+	
+	virtual void BeginPlay() override;
+	virtual void TickComponent(float DeltaTime, enum ELevelTick TickType, FActorComponentTickFunction* ThisTickFunction)override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* ia_LookUp;
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* ia_Turn;
+
+	// 좌우 회전 입력 처리
+	void Turn(const struct FInputActionValue& inputValue);
+	// 상하 회전 입력 처리
+	void LookUp(const struct FInputActionValue& inputValue);
+
+	virtual void SetupInputBinding(class UEnhancedInputComponent* PlayerInput) override;
+
+public:
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* ia_Move;
+
+	FVector direction;
+
+	void Move(const struct FInputActionValue& inputValue);
+
+	UPROPERTY(EditAnywhere, Category = PlayerSetting)
+	float walkSpeed = 300;
+
+	UPROPERTY(EditAnywhere, Category = PlayerSetting)
+	float runSpeed = 600;
+
+	void PlayerMove();
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* ia_Run;
+
+
+	// 달리기 이벤트 처리함수
+	void InputRun();
+
+
+
+	UPROPERTY(EditDefaultsOnly, Category = "Input")
+	class UInputAction* ia_Jump;
+
+	void InputJump(const struct FInputActionValue& inputValue);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	// 총을 쐈는지
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Shooting)
+	bool BShooting = false;
+
+	// 달리고 있는지
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Shooting)
+	bool BRunning = false;
+
+	// 앞으로 총을 두는 상태가 필요한지 체크 
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Shooting)
+	bool BIsRunShooting = false;
+
+	// 총 쏘고 전환시간
+	UPROPERTY(EditDefaultsOnly, BlueprintReadWrite, Category = Shooting)
+	float TransitionTime = 5.0f;
+
+	// 타이머 핸들 선언
+	FTimerHandle ThisHandle;
+
+	// 타이머 델리게이트 선언
+	FTimerDelegate ThisDelegate;
 };
